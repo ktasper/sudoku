66ae58556855c0a37bc3bb264a28a9a6d8de2817
Karl Webster <19327709+ktasper@users.noreply.github.com>
2022-04-25 22:38:51 +0100

    Update

--- puzzle/printer.py
+++ puzzle/printer.py
@@ -1,7 +1,15 @@
-def print_board_basic(grid):
-  # Format the grid
-  grid = [["?" if i == 0 else i for i in row] for row in grid]
-  # print the lists as strings with a space between each number
-  for row in grid:
-    line = (" ".join([str(i) for i in row]))
-    print(line)
+def print_board(bo):
+    bo = [["`" if i == 0 else i for i in row] for row in bo]
+    print("- - - - - - - - - - - - ")
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
